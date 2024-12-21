bc26ea67f4dcbaa00948ddce091034cec0e8e2a4
Sahil <sahilkhanofficial707@gmail.com>
2024-12-21 19:38:16 +0500

    777 change

--- currencyCalculator.dart
+++ currencyCalculator.dart
@@ -79,7 +79,7 @@ class _CurrencyConverterState extends State<CurrencyConverter> {
             DrawerHeader(
               child: Center(
                 child: Text(
-                  'Accad Assist 929',
+                  'Accad Assist 777',
                   style: TextStyle(
                     color: isDarkMode ? Colors.white : const Color(0xFF2A2A2A),
                     fontSize: 40,
