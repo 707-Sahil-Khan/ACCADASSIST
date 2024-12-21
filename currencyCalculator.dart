74172f6234ecf15e954dfcaabca3346ff0e909ef
Sahil <sahilkhanofficial707@gmail.com>
2024-12-21 18:14:28 +0500

    101

--- currencyCalculator.dart
+++ currencyCalculator.dart
@@ -79,7 +79,7 @@ class _CurrencyConverterState extends State<CurrencyConverter> {
             DrawerHeader(
               child: Center(
                 child: Text(
-                  'Accad Assist 1010',
+                  'Accad Assist 10102020',
                   style: TextStyle(
                     color: isDarkMode ? Colors.white : const Color(0xFF2A2A2A),
                     fontSize: 40,
